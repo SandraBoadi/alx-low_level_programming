190cfdd7273b563f96acffdaa413e53c7aec7ceb
SandraBoadi <boadisandra@yahoo.com>
2023-04-23 18:12:11 -0700

    Not yet

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,25 +1,32 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
+ * main - Source code to print the last digit
+ *
+ * Return: 0 (Success)
+ */
+
 int main(void)
 {
-	int n;
+int s;
+int b;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-
-if (n % 10 > 5)
-	printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
-else if (n % 10 == 0)
-	printf("Last digit of %d is %d and is 0\n", n, n % 10);
+srand(time(0));
+s = rand() - RAND_MAX / 2;
+b = s % 10;
+if (b > 5)
+{
+	printf("Last digit of %d is %d and greater than 5\n", s,b);
+}
+else if (b == 0)
+{
+	printf("Last digit of %d is %d and is less than 6 and not 0\n", s,b);
+}
 else
-	printf("Last digit of %d is %d and is less than 6 and not 0\n", n, n % 10);
+{
+	printf(Last digit of %d is %d and is less than 6 and not 0\n", s,b);
+}
 
 	return (0);
 }
-
---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: alphabbets in lower case
+ *
+ * Return: always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	char san
+
+for (san = 'x';san <= 'y';san++)
+{
+	putchar(san);
+}
+	purchar('\n');
+	return (0);
+}
+
+
+
+
+
+
+
