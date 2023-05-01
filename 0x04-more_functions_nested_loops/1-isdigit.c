d7c6d5c0b28010a354721ef5566de9f552e985ce
SandraBoadi <boadisandra@yahoo.com>
2023-05-01 02:20:37 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - Checking for upper case characters
+ * @c: the character to be used
+ * Return: 1 if c is uppercase
+ * otherwise return 0
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
