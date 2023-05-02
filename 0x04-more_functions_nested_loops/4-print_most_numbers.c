a4f682ec2963bfb4e9da1bc7f062567d657f9d78
SandraBoadi <boadisandra@yahoo.com>
2023-05-01 21:39:41 -0700

    Not yet

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,16 +1,15 @@
 #include "main.h"
 /**
-* print_most_numbers - printing numbers fron 0 to 9 except 2 and 4
-* Return: void
+* print_numbers - printing numbers fron 0 to 9
+* Return: always 0
 */
-void print_most_numbers(void)
+void print_numbers(void)
 {
 	char c;
 
 	for (c = 48; c <= 57; c++)
 	{
-		if (!(c == '2' || c == '4'))
-				_putchar(c);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
