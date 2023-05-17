bbce800da35cc3f932c9fd4697efbff6a25c047a
SandraBoadi <boadisandra@yahoo.com>
2023-05-17 04:30:10 -0700

    Not yet

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,84 +1,46 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
 /**
-		
- * str_concat - get ends of input and sum up together  for size
-		
+ * str_concat - get ends of input and add together for size
  * @s1: input one to concat
-		
  * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * Reyurn: concat of s1 and s2
  */
-		
-char *str_concat(char *s1, char *s2)
-		
+char *str_concat(char *s1, char *s2);
 {
-		
 	char *conct;
-		
-	int i, ci;
-		
+	int l, kl,
 
-		
 	if (s1 == NULL)
-		
-		s1 = "";
-		
+		    s1 = "";
+
 	if (s2 == NULL)
-		
 		s2 = "";
-		
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
+	l = kl = 0
+		while (s1[l] != '\0')
+		l++;
+
+	while (s2[kl] != '\0')
+		kl++;
 	conct = malloc(sizeof(char) * (i + ci + 1));
-		
 
-		
 	if (conct == NULL)
-		
 		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
+	k = kl = 0
+		while (s1[l] != '\0')
+		{
+			conct[l] = s1[l];
+			l++;
+		}
 
-		
-	while (s2[ci] != '\0')
-		
+	while (s2[kl] != '\0')
 	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+		conct[l] = s2[kl];
+		l++, kl++;
+
 	}
-		
-	conct[i] = '\0';
-		
+	conct[l] = '\0';
 	return (conct);
-		
 }
 
--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,34 +1,22 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include "main.h"
-
 /**
  * free_grid - frees 2d array
  * @grid: 2d grid
  * @height: height dimension of grid
  * Description: frees memory of grid
  * Return: nothing
-*/
-
+ *
+ */
 void free_grid(int **grid, int height)
-
 {
+	int s;
 
-	int l;
-
-
-
-	for (l = 0; l < height; l++)
-
+	for (s = 0; s < height; s++)
 	{
-
-		free(grid[l]);
-
+		free(grid[s]);
 	}
-
 	free(grid);
-
 }
 
