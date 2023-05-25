7bc6cbab99e0599cb2bc320c7846aeabd00ed7cc
SandraBoadi <boadisandra@yahoo.com>
2023-05-24 17:25:20 -0700

    Not yet

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,24 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - function that search
+ * @array - the array
+ * @size - element size in array
+ * @cmp - pointer to the function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	signed int m;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	for (m = 0; m < size; m++)
+
+		if (cmp(array[m]) != 0)
+		{
+			return (m);
+		}
+}
