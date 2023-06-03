f00df808bfe5cf5746544bf90e9c2bc7763f1403
SandraBoadi <boadisandra@yahoo.com>
2023-06-02 18:18:29 -0700

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,27 +1,26 @@
-i
-global_start
+section .data
+    fmt db '%s', 10, 0    ; format string
+    hello db 'Hello,', 0  ; string to print
+    holb db 'Holberton', 0 ; string to print
 
 section .text
+    extern printf        ; declare printf function from libc
 
-extern printf  ; declare printf function from libc
-
+global _start
 _start:
-move rax, 0	; return value used for printf
-move rprintdi, fmt	; address of format string
-move rsi, hello	; address of string to print
-call printf	; call printf
+    mov rax, 0           ; return value used for printf
+    mov rdi, fmt         ; address of format string
+    mov rsi, hello       ; address of string to print
+    xor rdx, rdx         ; clear rdx (optional if using 64-bit mode)
+    call printf          ; call printf
 
-move rax, 0	; return value used for printf
-move rdi, fmt	; address of format string
-move rsi, holb	; address of string to print
-call printf	; call printf
+    mov rax, 0           ; return value used for printf
+    mov rdi, fmt         ; address of format string
+    mov rsi, holb        ; address of string to print
+    xor rdx, rdx         ; clear rdx (optional if using 64-bit mode)
+    call printf          ; call printf
 
-move rax, 60	; system call number for exit
-xor rdi, rdi	; return value used for exit
-syscall		; call the kernel
+    mov rax, 60          ; system call number for exit
+    xor rdi, rdi         ; return value used for exit
+    syscall              ; call the kernel
 
-section .data
-
-fmt:	db '%s', 10, 0	; format string
-hello:	db 'Hello,', 0	; string to print
-holb:	db 'Holberton', 0	; string to print
