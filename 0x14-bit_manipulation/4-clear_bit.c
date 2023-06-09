a4b968b3dbd3a7657a8c88574f7944375baf9408
SandraBoadi <boadisandra@yahoo.com>
2023-06-09 08:45:00 -0700

    4-clear_bit.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -5,13 +5,15 @@
  * @n: pointer
  * @index: index
  *
- * Return: index
+ * Return: 10r -1
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index < 64)
 	{
 		*n = *n & ~(1 << index);
+		return (1);
 	}
+
 	return (-1);
 }
